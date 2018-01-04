dd3833959b19564c93d3d572c7c7bd4bbbf81f73
demarsis <demarsis@mail.ru>
2018-01-04 15:46:11 +0200

    Really fast!

--- simulation/penaltywaybuilder.cpp
+++ simulation/penaltywaybuilder.cpp
@@ -12,64 +12,70 @@ bool PenaltyWayBuilder::generate(FloorPtr floor)
     CellMatrixIterator it = floor->getCellIterator();
     while (it.hasNext())
     {
-        CellPtr currectCell = it.next();
+        CellPtr &currectCell = it.next();
         if (!currectCell) continue;
         currectCell->getAdditionalData().resetVisited();
     }
 
+    // cells positions need to be updated
+    std::deque<Position> currentToUpdate, nextToUpdate;
+
     // continue while have any changes
     bool wasChanges = true;
-    int currentVisitedValue = 1;
     while (wasChanges)
     {
+        ////////////////
         counter++;
         ConsoleLogger::info(QString("Attempt #") + QString::number(counter));
+        ////////////////
 
         wasChanges = false;
 
-        it.reset();
-        while (it.hasNext())
+        // fill the cells of finishes
+        currentToUpdate.swap(nextToUpdate);
+        nextToUpdate.clear();
+        const MapPositions &finishPoses = floor->getFinishMapPositions();
+        for (const Position &pos : finishPoses.getPositionList())
         {
-            CellPtr &currectCell = it.next();
+            currentToUpdate.push_back(pos);
+        }
+
+        // iterate all current cells need to be updated
+        for (const Position &currentCellPosition : currentToUpdate)
+        {
+            CellPtr &currectCell = floor->getCell(currentCellPosition);
             if (!currectCell) continue;
             CellAdditionalData &currentCellAdditionalData = currectCell->getAdditionalData();
-            const Position &currentCellPosition = currectCell->getPosition();
 
-            if (currentCellAdditionalData.visited < currentVisitedValue)
+            // for every neighbor
+            for (const Position &relativePos : relativeNeighPositions)
             {
-                currentCellAdditionalData.visited = currentVisitedValue;
+                // get a cell
+                Position neighPos(currentCellPosition.x + relativePos.x,
+                                  currentCellPosition.y + relativePos.y);
 
-                // for every neighbor
-                for (const Position &relativePos : relativeNeighPositions)
+                if (!floor->isValidPosition(neighPos)) continue;
+
+                CellPtr &neigh = floor->getCell(neighPos);
+                if (!neigh) continue;
+
+                CellAdditionalData &neighCellAdditionalData = neigh->getAdditionalData();
+
+                // count new penalty for current cell
+                Penalty newPenalty = currentCellAdditionalData.wayPenalty +
+                                     neighCellAdditionalData.cellPenalty *
+                                     sqrt((double)relativePos.x * (double)relativePos.x +
+                                          (double)relativePos.y * (double)relativePos.y);
+
+                // update only if the penalty is lower then the existing one
+                if (neighCellAdditionalData.wayPenalty > newPenalty)
                 {
-                    // get a cell
-                    Position neighPos(currentCellPosition.x + relativePos.x,
-                                      currentCellPosition.y + relativePos.y);
-
-                    if (!floor->isValidPosition(neighPos)) continue;
-
-                    CellPtr &neigh = floor->getCell(neighPos);
-                    if (!neigh) continue;
-
-                    CellAdditionalData &neighCellAdditionalData = neigh->getAdditionalData();
-
-                    // count new penalty for current cell
-                    Penalty newPenalty = currentCellAdditionalData.wayPenalty +
-                                         neighCellAdditionalData.cellPenalty *
-                                         sqrt((double)relativePos.x * (double)relativePos.x +
-                                              (double)relativePos.y * (double)relativePos.y);
-
-                    // update only if the penalty is lower then the existing one
-                    if (neighCellAdditionalData.wayPenalty > newPenalty)
-                    {
-                        neighCellAdditionalData.wayPenalty = newPenalty;
-                        wasChanges = true;
-                    }
+                    neighCellAdditionalData.wayPenalty = newPenalty;
+                    wasChanges = true;
+                    nextToUpdate.push_back(neigh->getPosition());
                 }
             }
         }
-
-        currentVisitedValue++;
     }
 
     return true;
