969577bc04c26738c6099c5cd790f77ce4a6a361
demarsis <demarsis@mail.ru>
2018-01-14 22:51:12 +0200

    Fixed a problem with report.

--- map/floorimage.cpp
+++ map/floorimage.cpp
@@ -3,6 +3,7 @@
 FloorImage::FloorImage(const QString &filename)
     : image(filename)
 {
+    image = image.convertToFormat(QImage::Format_ARGB32, image.colorTable());
 }
 
 const QImage &FloorImage::getImage() const
